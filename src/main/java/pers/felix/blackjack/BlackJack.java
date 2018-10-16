644575a7cb3616e8928cfbde3d93565d2f764c1a
Felix2048 <infinitesamsarax@outlook.com>
2018-10-16 13:39:11 +0800

    Environment Configuration

---
+++ src/main/java/pers/felix/blackjack/BlackJack.java
@@ -0,0 +1,14 @@
+package pers.felix.blackjack;
+
+/**
+ * @Title: BlackJack
+ * @Description: Black Jack Game
+ * @author: Felix
+ * @date: 10/16/2018 13:36
+ **/
+
+public class BlackJack {
+    public static void main(String[] args) {
+        System.out.println("This is a Black Jack game.");
+    }
+}
